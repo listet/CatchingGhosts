2289088100b5cd1ad9c48519f22dff7dd34e13b3
Alistair McCann <allyjmccann@gmail.com>
2024-01-29 22:08:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+window.addEventListener('load', () => {
+    //Här kickar ni igång ert program
+});
+
+
+function startGame (){
+console.log('startGame()');
+
+document.querySelector('#formDiv').classList.add('d-none');
+}
+
+
+function numberOfGhosts (){
+console.log('numberOfGhosts()');
+
+}
+
+
+function randomGhostPosition (){
+console.log('randomGhostPosition()');
+
+}
+
+
+function hoverOverGhost (){
+console.log('hoverOverGhost()');
+
+//skapa ett dolt element.
+
+//function som använder toggle för att byta emellan bilder. Klassen hide hämtas från cssfilen.
+
+
+//använd eventlisteners for att toggla mellan bilderna vid mouse hover.
+
+
+}
+
+
+function winGame (){
+console.log('winGame()');
+
+}
