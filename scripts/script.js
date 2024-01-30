68756253f80643772076f7e962d473c4d30b22d4
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2024-01-30 10:56:21 +0100

    test1

--- scripts/script.js
+++ scripts/script.js
@@ -44,7 +44,38 @@ function validateLogin(event) {
 
 
 function initContent() {
-    console.log('initContent')
+    console.log('success!');
     document.querySelector('#formDiv').classList.add('d-none');
 
+    // Slumpa antalet spöken mellan 10 och 15
+    let numGhosts = Math.floor(Math.random() * 6) + 10;
+
+    // For loop för att generera ett slumpmässigt antal spöken mellan 10-15.
+    for (let i = 0; i < numGhosts; i++) {
+        const ghost = document.createElement("img");
+        ghost.src = "./resources/ghost.png";
+        ghost.className = "ghost";
+        ghost.style.position = "absolute";
+        ghost.style.left = `${Math.random() * window.innerWidth}px`;
+        ghost.style.top = `${Math.random() * window.innerHeight}px`;
+        document.body.appendChild(ghost);
+
+        ghost.addEventListener('mouseover', (event) => {
+            toggleState(event.target);
+        });
+    }
+}
+
+
+
+function toggleState(target) {
+    if (target.classList.contains('ghost')) {
+        target.src = './resources/ghost.png';
+        target.classList.add('net');
+        target.classList.remove('ghost');
+    } else if (target.classList.contains('net')) {
+        target.src = './resources/net.png';
+        target.classList.remove('net');
+        target.classList.add('ghost');
+    }
 }
